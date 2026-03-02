046683f18ec2a1b59497fddcbf77e6d407600da5
Marlon Agner <140814301+marlonagner@users.noreply.github.com>
2026-03-02 23:09:58 +0000

    First Commit

---
+++ IConnectionString.cs
@@ -0,0 +1,4 @@
+public class IConnectionString
+{
+    string ConnectionString;
+}
---
+++ Service.cs
@@ -0,0 +1,12 @@
+public class Service
+{
+   public Service(){}
+
+   private IConnectionString connectionString;
+   public Service(IConnectionString connectionString)
+   {
+
+   }
+
+
+}
